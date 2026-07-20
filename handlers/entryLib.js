6a79c2243baed0c715a81cfc9e4950916cc42735
danibir <daniel.s.birkeland@gmail.com>
2026-07-20 02:56:27 +0200

    Improved datalists

--- handlers/entryLib.js
+++ handlers/entryLib.js
@@ -1,6 +1,29 @@
 const entryGet = require('./entryGet')
 
 const entryLib = async () => {
+    function countElm (arr, targetValue) {
+        return arr.filter(item => item === targetValue).length;
+    }
+    function sortByCount (arr) {
+        arr.sort((elm1, elm2)=>{
+            return countElm(arr, elm2) - countElm(arr, elm1)
+        })
+    }
+    function removeDups (arr) {
+        const seen = new Set()
+        const result = []
+        arr.forEach(elm => {
+            if (!seen.has(elm)) {
+                seen.add(elm)
+                result.push(elm)
+            }
+        })
+        return result
+    }
+    function getTopResults (arr) {
+        sortByCount(arr)
+        return removeDups(arr)
+    }
     const entries = await entryGet()
     const lib = {
         species: [],
@@ -16,6 +39,9 @@ const entryLib = async () => {
             }
         }
     }
+    lib.species = getTopResults(lib.species)
+    lib.origins = getTopResults(lib.origins)
+    lib.abilities = getTopResults(lib.abilities)
     return lib
 }
 
